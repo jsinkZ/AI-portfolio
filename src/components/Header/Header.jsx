fffddb63447f3989247e745aceafb92276a76f84
jsink_ <andrikv@gmail.com>
2022-07-14 06:27:27 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./Header.module.scss"
+
+const Header = (props) => {
+    return (
+        <header> Header </header>
+    )
+}
+
+export default Header
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./Main.module.scss"
+
+const Main = (props) => {
+    return (
+        <main> main </main>
+    )
+}
+
+export default Main
